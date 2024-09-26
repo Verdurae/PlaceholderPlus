77517edc1d9565b75309dd546452f2763575cc2c
Kaminy <1713483284@qq.com>
2024-09-27 00:13:45 +0800

    Initial commit

---
+++ src/main/java/org/verdurae/placeholderplus/PlaceholderPlus.java
@@ -0,0 +1,17 @@
+package org.verdurae.placeholderplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlaceholderPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
